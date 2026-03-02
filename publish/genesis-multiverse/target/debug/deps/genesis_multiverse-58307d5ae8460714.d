b6898de1e2d7c88eecaf35821886d6116917fd38
Kevan <kevan@yourdomain.com>
2026-03-02 05:08:15 -0500

    Add .gitignore to publish stub, remove target artifacts from tracking

--- publish/genesis-multiverse/target/debug/deps/genesis_multiverse-58307d5ae8460714.d
+++
@@ -1,9 +0,0 @@
-C:\Users\Kevan\Genesis\publish\genesis-multiverse\target\debug\deps\genesis_multiverse-58307d5ae8460714.d: src\lib.rs
-
-C:\Users\Kevan\Genesis\publish\genesis-multiverse\target\debug\deps\libgenesis_multiverse-58307d5ae8460714.rlib: src\lib.rs
-
-C:\Users\Kevan\Genesis\publish\genesis-multiverse\target\debug\deps\libgenesis_multiverse-58307d5ae8460714.rmeta: src\lib.rs
-
-src\lib.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.1.0
